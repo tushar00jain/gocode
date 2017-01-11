8131a2d4a981fa6f9fb05ee4c7dad5051cbec824
Tushar Jain <jain00tushar@gmail.com>
2017-01-11 00:36:38 +0000

    Tryig new things

--- hello/hello.go
+++ hello/hello.go
@@ -2,10 +2,36 @@ package main
 
 import (
 	"fmt"
-	"github.com/tushar00jain/gocode/string"
+	// "github.com/tushar00jain/gocode/string"
 )
+
+type Person struct {
+	Name string
+}
+
+type Saiyan struct {
+	*Person
+	Power int
+}
 
 func main() {
 	// fmt.Printf("Hello, world.\n")
-	fmt.Println(string.Reverse("Hello, world."))
+	// fmt.Println(string.Reverse("Hello, world."))
+
+	goku := &Saiyan{&Person{"Goku"}, 9000}
+	goku.Introduce()
+	goku.Person.Introduce()
+
+}
+
+func Super(s *Saiyan) {
+	s = &Saiyan{&Person{"Gohan"}, 1000}
+}
+
+func (p *Person) Introduce() {
+	fmt.Println("Person", p.Name)
+}
+
+func (s *Saiyan) Introduce() {
+	fmt.Println("Saiyan", s.Name)
 }
